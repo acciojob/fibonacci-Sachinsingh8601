ecf7068ace1fa2c875061817956521dea540c50e
sachin kumar singh <sachinjasingh@gmail.com>
2025-06-19 22:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function fibonacci(num) {
+	function fibonacci(num) {
+  if (num === 1) return 0;
+  if (num === 2) return 1;
+
+  let a = 0, b = 1, fib;
+
+  for (let i = 3; i <= num; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+
+  return b;
+}
+
+	
 // your code here
 }
 
